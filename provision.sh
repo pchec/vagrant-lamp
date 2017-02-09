b3350a888288b5a305f66ef8c163fa851407f5dc
Piotr Chęć <piotr.chec@gmail.com>
2017-02-09 01:05:39 +0100

    Added php-ldap extension for Apache.

--- provision.sh
+++ provision.sh
@@ -86,7 +86,7 @@ EOF
 }
 
 php_go() {
-	apt-get -y install php5 php5-curl php5-mysql php5-sqlite php5-xdebug php5-intl
+	apt-get -y install php5 php5-curl php5-mysql php5-sqlite php5-xdebug php5-intl php5-ldap
 
 	sed -i "s/display_startup_errors = Off/display_startup_errors = On/g" ${php_config_file}
 	sed -i "s/display_errors = Off/display_errors = On/g" ${php_config_file}
